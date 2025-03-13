9cc3af0c1d5340c7e7cf1e5389ec234d66503d4e
aidas777-ctrl <fadhila2903@gmail.com>
2025-03-14 00:09:32 +0700

    Praktikum_5

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
 
 Route::get('/', [HomeController::class, 'index']);
@@ -22,3 +23,5 @@ Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
 Route::get('/sales', [SalesController::class, 'index']);
 
 Route::get('/level', [LevelController::class, 'index']);
+
+Route::get('/kategori', [KategoriController::class, 'index']);
