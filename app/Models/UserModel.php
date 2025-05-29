89b31e7d3532b7e97042ae4c3981db4a1d62d3f1
aidas777-ctrl <fadhila2903@gmail.com>
2025-05-29 22:36:53 +0700

    Praktikum1_JS11_RESTFUL_API

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Relations\BelongsTo; // Tambahkan ini
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Models\LevelModel;
 
@@ -25,7 +26,7 @@ class UserModel extends Authenticatable implements JWTSubject
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
-    protected $fillable = ['username', 'password', 'nama', 'level_id',  'created_at', 'updated_at'];
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'image', 'created_at', 'updated_at'];
     protected $hidden = ['password'];
 
     protected $casts = ['password' => 'hashed'];
@@ -33,6 +34,13 @@ class UserModel extends Authenticatable implements JWTSubject
     public function level()
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts' . $image),
+        );
     }
 
     public function getRoleName(): string
@@ -45,8 +53,8 @@ class UserModel extends Authenticatable implements JWTSubject
         return $this->level->level_kode == $role;
     }
 
-    public function getRole(){
+    public function getRole()
+    {
         return $this->level->level_kode;
     }
 }
-
